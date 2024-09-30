fdaa6be553d8d216da8e05c315404546eb4901f2
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-30 11:01:46 -0300

    feat(routes): configure routing with initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Info from "./pages/Info";
+
 const App = () => {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="/" index element={<Info />} />
+          <Route path='*' element={<Info />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Info/index.tsx
@@ -0,0 +1,9 @@
+const Info = () => {
+  return <>
+    <h1>
+      Página Info
+    </h1>
+  </>
+}
+
+export default Info;
