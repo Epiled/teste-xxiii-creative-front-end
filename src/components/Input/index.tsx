d418620ac2bf5511c3c5dcd380040e3f0c4d72bf
Felipe De Andrade <felipe.deam98@gmail.com>
2024-10-01 05:07:07 -0300

    feat(EditProfile): complete components and styles for profile edit page

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,31 +1,45 @@
+import { useState } from 'react';
 import styles from './Input.module.scss';
 
 interface IProp {
   id: string;
   label: string;
   name: string;
-  tipo?: string;
+  type?: string;
+  tipe?: string;
+  value?: string;
 }
 
 interface ISelect extends Omit<IProp, 'name'> {
   values: string[];
 }
 
-const Input = ({ id, label, name, tipo = 'text' }: IProp) => {
+const Input = ({ id, label, name, type = 'text', tipe, value = '' }: IProp) => {
+  const [valueIpt, setValueIpt] = useState(value);
+
   return (
     <fieldset className={styles.input}>
-      <label
-        className={styles.input__label}
-        htmlFor={id}
-      >
-        {label}
-        <input
-          className={styles.input__ipt}
-          id={id}
-          name={name}
-          type={tipo}
-        />
-      </label>
+      <div className={styles.input__top}>
+        <label
+          className={styles.input__label}
+          htmlFor={id}
+        >
+          {label}
+        </label>
+
+        {tipe && <div className={styles.input__tipe}>
+          <img className={styles.input__iconTipe} src='/icons/info.png' />
+          <span className={styles.input__txt}>{tipe}</span>
+        </div>}
+      </div>
+      <input
+        className={styles.input__ipt}
+        id={id}
+        name={name}
+        type={type}
+        value={valueIpt}
+        onChange={(e) => setValueIpt(e.target.value)}
+      />
     </fieldset>
   )
 }
--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from '../../components/Buttons';
 import Header from '../../components/Header';
+import { Input } from '../../components/Input';
 import PageTitles from '../../components/PageTitles';
 import styles from './EditProfile.module.scss'
 
@@ -12,15 +13,57 @@ const EditProfile = () => {
       <PageTitles title={title} url='' />
 
       <main className={styles.editProfile}>
-        <Button />
-        <Button type={'reverse'} />
-        <Button type={'gradient'} />
-        <Button type={'blank'} />
-      </main>
 
-      <h1>
-        Página Edit Profile
-      </h1>
+        <div className={styles.editProfile__ipts}>
+          <Input
+            id='nome'
+            label='Name'
+            name='nome'
+            tipe="Dica Lorem Impsum"
+            value='Scarlet'
+          />
+
+          <div>
+            <Input
+              id='username'
+              label='Username'
+              name='username'
+              tipe="Dica Lorem Impsum"
+              value='scarlet.roses'
+            />
+            <span className={styles.editProfile__txt}>
+              In most cases, you’ll be able to change your name back to <span className={styles[`editProfile__txt--desc`]}>scarlet.roses</span> for another 14 days.
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.editProfile__preview}>
+          <div className={styles.editProfile__top}>
+            <h3 className={styles.editProfile__title}>Profile</h3>
+            <div className={styles.editProfile__tipe}>
+              <img className={styles.editProfile__iconTipe} src='/icons/info.png' />
+              <span className={styles.editProfile__previewtxt}>Lorem Impsum</span>
+            </div>
+          </div>
+          <a href="" className={styles.editProfile__url}>
+            https://roses.vip/<span className={styles[`editProfile__url--desc`]}>scarlet.roses</span>
+          </a>
+        </div>
+
+        <div className={styles.editProfile__notification}>
+          <span>
+            <h3>Identity Verification</h3>
+            <img src="/icons/check-black.png" alt="" width={24} height={24} />
+          </span>
+          <p>Let’s make sure nobody is trying to impersonate you.</p>
+          <Button text={'Verify Account'} type={'reverse'} />
+        </div>
+
+        <div className={styles.editProfile__btns}>
+          <Button text={'Done'} type={'gradient'} />
+          <Button text={'Cancel'} type={'blank'} />
+        </div>
+      </main>
     </>
   )
 }
